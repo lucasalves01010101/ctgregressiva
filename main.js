168202c297fb751009cc51ac90bc8977c4d6932f
Lucas Alves <lucasalves112008@gmail.com>
2020-12-26 17:07:11 -0300

    adicionando arquivos do projeto

---
+++ main.js
@@ -0,0 +1,37 @@
+const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
+const nextYearContainer = document.querySelector('#year')
+
+const newYearTime  = new Date(`January 2021 00:00:00`)
+
+nextYearContainer.textContent = "2021"
+
+const updateCountdown = () => {
+    const currentTime = new Date()
+    const diferrence = newYearTime - currentTime
+    if(diferrence <= 0){
+        const contador = document.querySelector('#countdown')
+        contador.style.display="none"
+
+        const texto = document.querySelector('#h1')
+        texto.innerText = "Deu certo"
+
+    }
+    const days = Math.floor(diferrence / 1000 / 60 / 60 / 24)
+    const hours = Math.floor(diferrence / 1000 / 60 / 60 ) % 24
+    const minutes = Math.floor(diferrence / 1000 / 60 ) % 60
+    const seconds = Math.floor(diferrence / 1000 ) % 60
+
+    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+    minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+    hoursContainer.textContent = hours < 10 ? '0' + hours : hours
+    daysContainer.textContent = days < 10 ? '0' + days : days 
+
+    
+
+
+}
+
+setInterval(updateCountdown , 1000)
